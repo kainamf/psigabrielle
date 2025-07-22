84efcc1c8bca8d9f8b0f66c83a3bd7bdb18e4d7f
kainamf <kaina.marques@calindra.com.br>
2025-07-21 23:37:40 -0300

    refactor: remove Testimonials component and update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
-import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
@@ -20,7 +19,6 @@ function App() {
             <Hero />
             <Services />
             <About />
-            <Testimonials />
             <Contact />
             <Footer />
             <WhatsAppButton />
