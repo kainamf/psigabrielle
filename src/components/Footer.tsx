349b80a8155dbad7cf8844dbb03fdb5fa3239d0e
kainamf <kaina.marques@calindra.com.br>
2025-07-23 23:51:27 -0300

    fix: update copyright owner in Footer component to Kainã Freitas

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ function Footer() {
                     </div>
                 </div>
                 <div className="border-t border-primary mt-8 pt-8 text-center text-secondary">
-                    <p>&copy; 2025 Gabrielle Mazulo - Todos os direitos reservados</p>
+                    <p>&copy; 2025 Kainã Freitas - Todos os direitos reservados</p>
                     <p className="text-xs mt-2">Desenvolvido por <a href="https://github.com/kainamf" target="_blank" rel="noopener noreferrer" className="underline hover:text-white">Kainã Freitas</a></p>
                 </div>
             </div>
